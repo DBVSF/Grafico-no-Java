276bcbb608ffa06702dfe28ecbbdc575d821eef8
Darlan Bueno <darlan_bueno@hotmail.com>
2019-07-17 18:12:33 -0300

    preparando terreno pra por sprint

--- Grafico/src/graficos/Game.java
+++ Grafico/src/graficos/Game.java
@@ -4,9 +4,12 @@ package graficos;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 
@@ -15,18 +18,20 @@ public class Game extends Canvas implements Runnable{
     public static JFrame frame;
     private Thread thread;
     private boolean isRunning = true;
-    private final int WIDTH = 160;
-    private final int HEIGHT = 120;
+    private final int WIDTH = 240;
+    private final int HEIGHT = 160;
     private final int SCALE = 3;
     
     private BufferedImage image;
     
     
+  
    
     
     
     
     public Game (){
+        
         setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
         initFrame();
         image = new BufferedImage(WIDTH,HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -58,7 +63,16 @@ public class Game extends Canvas implements Runnable{
         
     public synchronized void stop (){
         
+        //parar caso dê erro
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
     }
+       
     
     public static void main(String[] args) {
         Game game = new Game();
@@ -79,6 +93,12 @@ public class Game extends Canvas implements Runnable{
         //cor do backgroud
         g.setColor(new Color(30,30,30));
         g.fillRect(0, 0, WIDTH, HEIGHT);
+        g.setFont(new Font("Arial",Font.BOLD,20));
+        g.setColor(Color.white);
+        g.drawString("OOOOOOO",19, 19);
+        g.setColor(Color.MAGENTA);
+        g.fillRect(20, 20, 80, 80);
+        
         g = bs.getDrawGraphics();
         //preenchendo o background
         g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
@@ -104,7 +124,7 @@ public class Game extends Canvas implements Runnable{
             }
             
         }
-        
+        stop();
     }
     
     
